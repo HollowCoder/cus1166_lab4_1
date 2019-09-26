326b84bb3817dd478c59a99a7da941f199854e3a
Andrew <andrew.oleary13@stjohns.edu>
2019-09-26 12:48:48 -0400

    Changes made.

--- playground/app.py
+++ playground/app.py
@@ -1,3 +1,4 @@
+from app import app
 from flask import Flask, render_template, request
 #from flask_sqlalchemy import SQLAlchemy
 
@@ -21,3 +22,6 @@ def welcome():
 @app.route("/roster/<int:grade_view>")
 def roster():
     return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
+
+if __name__ == '__main__':
+    app.run()
