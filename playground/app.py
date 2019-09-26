836076f0695632f88b097ef29cbfdbc63968a907
Andrew <andrew.oleary13@stjohns.edu>
2019-09-26 19:10:15 -0400

    Almost done.

--- playground/app.py
+++ playground/app.py
@@ -19,7 +19,7 @@ def welcome():
     return render_template("welcome.html", student_name=student_name)
 
 @app.route("/roster/<int:grade_view>")
-def roster():
+def roster(grade_view):
     return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
 
 if __name__ == '__main__':
